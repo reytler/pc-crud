963c4dac110c2507e689c16f446b995414afc823
Reytler Souza <reytlercompare@hotmail.com>
2024-06-05 11:20:37 -0300

    Add feat validar cpf

--- Utils/Utils.cs
+++ Utils/Utils.cs
@@ -1,7 +1,45 @@
 ﻿namespace crudpcapi.Utils;
 
-public class Utils
+public static class Utils
 {
+    public static bool ValidateCpf(string cpf)
+    {
+        cpf = new string(cpf.Where(char.IsDigit).ToArray());
+
+        if (cpf.Length != 11)
+            return false;
+
+        var multiplicador1 = new int[9] { 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+        var multiplicador2 = new int[10] { 11, 10, 9, 8, 7, 6, 5, 4, 3, 2 };
+        var tempCpf = cpf.Substring(0, 9);
+        var soma = 0;
+
+        for (int i = 0; i < 9; i++)
+            soma += int.Parse(tempCpf[i].ToString()) * multiplicador1[i];
+
+        var resto = soma % 11;
+        if (resto < 2)
+            resto = 0;
+        else
+            resto = 11 - resto;
+
+        var digito = resto.ToString();
+        tempCpf += digito;
+        soma = 0;
+
+        for (int i = 0; i < 10; i++)
+            soma += int.Parse(tempCpf[i].ToString()) * multiplicador2[i];
+
+        resto = soma % 11;
+        if (resto < 2)
+            resto = 0;
+        else
+            resto = 11 - resto;
+
+        digito += resto.ToString();
+
+        return cpf.EndsWith(digito);
+    }
 }
 
 public static class ExtensionMethods
