6ffa514da9ac61a93ae703d9a793d6f45458008f
Reytler Souza <reytlercompare@hotmail.com>
2024-06-05 11:21:24 -0300

    Add Encryption service

---
+++ Services/EncryptionService.cs
@@ -0,0 +1,76 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace crudpcapi.Services;
+
+public class EncryptionService
+{
+    private readonly byte[] _key;
+    private readonly byte[] _iv;
+
+    public EncryptionService(string key)
+    {
+        using (var sha256 = SHA256.Create())
+        {
+            _key = sha256.ComputeHash(Encoding.UTF8.GetBytes(key));
+        }
+        _iv = new byte[16]; // Inicializa com zeros
+    }
+
+    public string Encrypt(string plainText)
+    {
+        using (var aes = Aes.Create())
+        {
+            aes.Key = _key;
+            aes.IV = _iv;
+
+            var encryptor = aes.CreateEncryptor(aes.Key, aes.IV);
+            using (var ms = new MemoryStream())
+            {
+                using (var cs = new CryptoStream(ms, encryptor, CryptoStreamMode.Write))
+                {
+                    using (var sw = new StreamWriter(cs))
+                    {
+                        sw.Write(plainText);
+                    }
+                }
+                return Convert.ToBase64String(ms.ToArray());
+            }
+        }
+    }
+
+    public string Decrypt(string cipherText)
+    {
+        using (var aes = Aes.Create())
+        {
+            aes.Key = _key;
+            aes.IV = _iv;
+
+            var decryptor = aes.CreateDecryptor(aes.Key, aes.IV);
+            using (var ms = new MemoryStream(Convert.FromBase64String(cipherText)))
+            {
+                using (var cs = new CryptoStream(ms, decryptor, CryptoStreamMode.Read))
+                {
+                    using (var sr = new StreamReader(cs))
+                    {
+                        return sr.ReadToEnd();
+                    }
+                }
+            }
+        }
+    }
+
+    public static string GerarHash(string input)
+    {
+        MD5 md5Hash = MD5.Create();
+        byte[] data = md5Hash.ComputeHash(Encoding.UTF8.GetBytes(input));
+        StringBuilder sBuilder = new StringBuilder();
+
+        for (int i = 0; i < data.Length; i++)
+        {
+            sBuilder.Append(data[i].ToString("x2"));
+        }
+
+        return sBuilder.ToString();
+    }
+}
